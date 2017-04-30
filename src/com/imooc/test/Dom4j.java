3b4bb5b375354b58d7b069dea4401d5b846aaf48
madison <zhuangsen@live.com>
2017-04-30 21:58:54 +0800

    update

--- src/com/imooc/test/Dom4j.java
+++ src/com/imooc/test/Dom4j.java
@@ -1,9 +1,8 @@
 package com.imooc.test;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,23 +11,28 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 import org.junit.Test;
 
 public class Dom4j implements XmlDocument {
-	
+
 	@Test
 	@Override
-	public void test(){
-		createXml("");
-		parseXml("");
+	public void test() {
+		createXml("employees.xml");
+		createMyXml("rssnews.xml");
+		parseXml("employees.xml");
 	}
 
 	@Override
 	public void createXml(String fileName) {
+		// 1.创建document对象，代表整个xml文档
 		Document document = DocumentHelper.createDocument();
+		// 2.创建根节点
 		Element employees = document.addElement("employees");
+		// 3.生成子节点及节点内容
 		Element employee = employees.addElement("employee");
 		Element name = employee.addElement("name");
 		name.setText("madison");
@@ -36,11 +40,45 @@ public class Dom4j implements XmlDocument {
 		sex.setText("男");
 		Element age = employee.addElement("age");
 		age.setText("18");
+
+		// 5.设置生成xml的格式
+		OutputFormat format = OutputFormat.createPrettyPrint();
+		format.setEncoding("UTF-8");
 		try {
-			Writer fileWriter = new FileWriter(fileName);
-			XMLWriter xmlWriter = new XMLWriter(fileWriter);
+			File file = new File(fileName);
+			XMLWriter xmlWriter = new XMLWriter(new FileOutputStream(file), format);
+			// Writer fileWriter = new FileWriter(fileName);
+			// XMLWriter xmlWriter = new XMLWriter(fileWriter,format);
 			xmlWriter.write(document);
 			xmlWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void createMyXml(String fileName) {
+		// 1.创建document对象，代表整个xml文档
+		Document document = DocumentHelper.createDocument();
+		// 2.创建根节点rss
+		Element rss = document.addElement("rss");
+		// 3.向rss节点中添加version属性
+		rss.addAttribute("version", "2.0");
+		// 4.生成子节点及节点内容
+		Element channel = rss.addElement("channel");
+		Element title = channel.addElement("title");
+		title.setText("<![CDATA[上海移动互联网产业促进中心正式揭牌 ]]>");
+		// 5.设置生成xml的格式
+		OutputFormat format = OutputFormat.createPrettyPrint();
+		format.setEncoding("GBK");
+		// 6.生成xml文件
+		File file = new File(fileName);
+		XMLWriter writer;
+		try {
+			writer = new XMLWriter(new FileOutputStream(file), format);
+			// 设置是否转义，默认值是true，代表转义
+			writer.setEscapeText(false);
+			writer.write(document);
+			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
